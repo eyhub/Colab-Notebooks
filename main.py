94fd568ff4b6a2d586f4d03127503bd836425ac1
öbgürü <ozgurakbulut2@gmail.com>
2025-01-26 23:05:26 +0100

    ssss

--- main.py
+++
@@ -1,8 +0,0 @@
-from config import API_TOKEN
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-
-TOKEN = API_TOKEN
-BOT_USERNAME: Final = "eyai_bot"
-
-                         3
